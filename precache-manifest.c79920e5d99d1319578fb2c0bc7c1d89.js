04f243b92f174ce7cffe92efdfb19bb200b2d30f
Prabhjyot Singh <prabhjyotg@gmail.com>
2020-09-07 16:23:15 -0400

    Updates

---
+++ precache-manifest.c79920e5d99d1319578fb2c0bc7c1d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82be93eed2a916567a898c5f8b2c369",
+    "url": "/Spotify-Clone/index.html"
+  },
+  {
+    "revision": "047fb58cd8ef4f6bf159",
+    "url": "/Spotify-Clone/static/css/main.e0612ba1.chunk.css"
+  },
+  {
+    "revision": "23a255b6d5e98b6aabf8",
+    "url": "/Spotify-Clone/static/js/2.552f7bc5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Spotify-Clone/static/js/2.552f7bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047fb58cd8ef4f6bf159",
+    "url": "/Spotify-Clone/static/js/main.def6d011.chunk.js"
+  },
+  {
+    "revision": "0f68c80e83b543c97620",
+    "url": "/Spotify-Clone/static/js/runtime-main.0c1c7ffe.js"
+  }
+]);
